836522a2d7863bd7cf2b2f221dcbf103dd0ca42a
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2019-04-30 23:36:20 -0500

    Rascunho carregamento datasus no R

---
+++ datasuspoliticasocial.R
@@ -0,0 +1,7 @@
+#Indicadores de Saúde - Acesso ao datasus
+require(devtools)
+#repo original em : rpradosiqueira/datasus
+#fork para adicionar consulta à pirâmide etária
+#install_github("rodrigoesborges/datasus")
+
+library(datasus)
