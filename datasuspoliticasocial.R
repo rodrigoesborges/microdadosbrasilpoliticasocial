adcdfbb8adbc67c8f418a3939ba9ec279e73bde4
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2019-05-06 22:02:22 -0500

    teste utilização do pacote datasus ampliado

--- datasuspoliticasocial.R
+++ datasuspoliticasocial.R
@@ -2,6 +2,8 @@
 require(devtools)
 #repo original em : rpradosiqueira/datasus
 #fork para adicionar consulta à pirâmide etária
-#install_github("rodrigoesborges/datasus")
+#necessário instalar novamente quando novas funções de coleta forem adicionadas
+install_github("rodrigoesborges/datasus")
 
-library(datasus)
+require(datasus)
+popes <- novapop_popbr_mun(unidade_da_federacao = "Espírito Santo")
