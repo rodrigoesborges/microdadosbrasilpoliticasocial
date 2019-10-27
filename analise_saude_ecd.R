e1beac187611daa67b5cabd06175dc85da3f167b
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2019-10-27 13:25:38 -0500

    indicadores de saúde de primeira infância, primeiras incursões programáticas

---
+++ analise_saude_ecd.R
@@ -0,0 +1,23 @@
+#Requisitos
+#Pacote datasus
+require(datasus) 
+        
+
+#Estimativas de população 
+pop_m_inf <- read.csv2("data/")
+
+
+
+
+#indicadores de saúde de primeira infânci
+#A)Mortalidade no parto
+
+#B) Tipo de Parto
+
+
+#C) Nascidos Vivos idade m
+
+
+#D)Mortalidade Faixa etária x mun
+
+
