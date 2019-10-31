4f9317e9793669af6e5c0275c8d3f1be16b71dcb
rodrigoesborges <rodrigoesborges@gmail.com>
2019-10-31 11:53:49 -0300

    correção estimativas não beneficiários pbf

--- analisecadunico-polsocial-es-ecd.R
+++ analisecadunico-polsocial-es-ecd.R
@@ -57,14 +57,15 @@ calc_ind_cu <- function(ano = 2017, uf = 32) {
     #db <- dbConnect( MonetDBLite() , cadunicoecd$db$dbname).
     #cadunicoecd <- dbGetQuery(db , paste0( "SELECT * FROM pessoa_" ,ano," WHERE idade < 7" ) )
     #dbGetQuery( db , "SELECT stype , dnum , cname , SUM( pw ) FROM apiclus1 GROUP BY stype , dnum , cname" ) )
-      ecdpbfmun <- svyby(~I(marc_pbf == 1),~cd_ibge,cadunico_ecd, svytotal,na.rm = TRUE )
+      ecdpbfmun <- svyby(~I(marc_pbf > 0),~cd_ibge,cadunico_ecd, svytotal,na.rm = TRUE )
 
       #Compatibiliza com códigos do datasus
       ecdpbfmun$cd_ibge <- substr(ecdpbfmun$cd_ibge,1,6)
       
       names(ecdpbfmun) <- c("cod_mun","n_pbf_cadunico", "ben_pbf", "desv_pad_n_pbf", "desv_pad_pbf")
       
-      ecdpbfmun <- ecdpbfmun %>% right_join(popm_inf[popm_inf$ano == ano  & grepl(sprintf("%s",ufc),popm_inf$cod_mun),c(1,2,7)], by = "cod_mun") 
+      ecdpbfmun <- ecdpbfmun %>% 
+        right_join(popm_inf[popm_inf$ano == ano  & grepl(sprintf("%s",ufc),popm_inf$cod_mun),c(1,2,7)], by = "cod_mun") 
         
       ecdpbfmun <- ecdpbfmun %>% mutate("prop_pinf_cadunico" = (n_pbf_cadunico+ben_pbf)/pop0a6, "pbf_pop0a6" = ben_pbf/pop0a6, ano = ano)
       
@@ -74,6 +75,16 @@ calc_ind_cu <- function(ano = 2017, uf = 32) {
 
 tab_ecd_cadunico <- rbindlist(lapply(anosel, calc_ind_cu))
 saveRDS(tab_ecd_cadunico, "data/2012-2017-indicadores-cadunico-ES.rds", compress = "gzip")
+
+###Adaptar para calcular indicadores de todos os municípios, estado a estado
+# combosf <-  expand.grid(anosel,0:9)
+# names(combosf) <- c("dano","idade")
+# 
+# #Problema com a funçao - 0 a 1
+# pop_var <- rbindlist(lapply(1:nrow(combosf),
+#                             function(x) do.call(mapply,c("calc_id",combosf))[,x]))
+
+
 
 #### Resumo dos dados do Espírito Santo - Estado - Cadunico
 
